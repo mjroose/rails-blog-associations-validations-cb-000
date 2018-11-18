994c8afbdbaeab75b9302f3b422917b956701792
Michael Roose <mjroose@gmail.com>
2018-11-18 00:36:32 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_tags, only: [:new, :edit]
 
   # GET /posts
   # GET /posts.json
@@ -65,6 +66,10 @@ class PostsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
+    end
+
+    def set_tags
+      @tags = Tag.all
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
